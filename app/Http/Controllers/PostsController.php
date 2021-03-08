56309af76ff3476cb04d54fb824bbc6381fb4db1
Abyravka90 <asepcahya.nugraha75@gmail.com>
2021-03-08 11:31:12 +0700

    edit Data fixed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+//jangan lupa pakai ini
 use Illuminate\Http\Request;
 use App\Post;
 use illuminate\Support\Str;
@@ -14,12 +15,12 @@ class PostsController extends Controller
         $posts = Post::latest()->get();
         return view('posts.index', compact('posts'));
     }
-
+    //menampilkan form input
     public function create()
     {
         return view('posts.create');
     }
-
+    //menyimpan data ke database
     public function store(Request $request)
     {
         $post = Post::create([
@@ -30,4 +31,21 @@ class PostsController extends Controller
 
         return redirect(route('posts.index'));
     }
+    //menampilkan form edit 
+    public function edit(Request $request, Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+    //mengupdate data ke database
+    public function update(Request $request, Post $post)
+    {
+        $post = Post::whereId($post->id)->update([
+            'title' => $request->input('title'), 
+            'slug' => Str::slug($request -> input('title')),
+            'content' => $request->input('content')
+        ]);
+
+        return redirect(route('posts.index'));
+    }
+
 }
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Edit Post</title>
+</head>
+<body>
+    <div class="container" style="margin-top: 80px">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="card">
+                    <div class="card-header">
+                        Tambah Post
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('posts.update', $post->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input value="{{ $post -> title }}" type="text" name="title" id="title" class="form-control" placeholder="Masukan Title">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="content">Title</label>
+                            <textarea name="content" id="content" class="form-control" placeholder="Masukan Title" rows="4">{{ $post -> content }}</textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">simpan</button>
+                        <button type="reset" class="btn btn-warning">reset</button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace( 'content' );
+</script>
+</body>
+</html>
